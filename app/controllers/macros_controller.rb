bd55e097661209a81c56b1b3790c660320498517
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-28 20:07:45 +0000

    response is working

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -7,6 +7,59 @@ class MacrosController < ApplicationController
   def process_inputs
     @the_image = params.fetch("image_param", nil)
     @the_description = params.fetch("description_param")
+    
+    c = OpenAI::Chat.new
+    c.model = "o3"
+    c.system("You are an expert nutritionist. The user will give you an image and/or a description of a meal. Your job is to estimate the macronutrients and calories in it.")
+    c.user("Here is an image:", image: @the_image)
+    c.user(@the_description)
+    c.schema = '{
+      "name": "nutrition_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "number",
+            "description": "Amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "number",
+            "description": "Amount of protein in grams."
+          },
+          "fat": {
+            "type": "number",
+            "description": "Amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "number",
+            "description": "Total calories in kcal."
+          },
+          "notes": {
+            "type": "string",
+            "description": "A breakdown of how you arrived at the values, and additional notes."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories",
+          "notes"
+        ],
+        "additionalProperties": false
+      },
+      "strict": true
+    }'
+
+    @structured_output = c.assistant!
+
+    @g_carbs = @structured_output.fetch("carbohydrates")
+    @g_protein = @structured_output.fetch("protein")
+    @g_fat = @structured_output.fetch("fat")
+    @kcal = @structured_output.fetch("total_calories")
+    @notes = @structured_output.fetch("notes")
+
+    c.assistant!
 
     render({ :template => "macro_templates/results" })
   end
